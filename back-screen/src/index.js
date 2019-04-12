7f09d0afea6754f5b1fd716fe3d8219d2d442413
James <jameselson19@gmail.com>
2019-04-12 23:18:50 +0100

    maccies fam

--- back-screen/src/index.js
+++ back-screen/src/index.js
@@ -1,30 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import localAdImg from './localAdImg.png';
 
-function BusTimeComp(props) {
+console.log(localAdImg);
+
+function mapComp(props) {
   return <h1>{props.name + " " + props.age}</h1>;
+}
+
+function localAd(props){
+  return <img src = {props.localAdImg} alt = "localAdImg" />;
 }
 
 function App() {
   return (
     <div>
     <div className="map">
-    <BusTimeComp name="1" age="69"/>
-    <BusTimeComp name="2" age = "4"/>
-    <BusTimeComp name="3" age="6"/>
+    <mapComp name="1" age="69"/>
+    <mapComp name="2" age = "4"/>
+    <mapComp name="3" age="6"/>
     </div>
 
     <div className="localAd">
-    <BusTimeComp name="1" age="69"/>
-    <BusTimeComp name="2" age = "4"/>
-    <BusTimeComp name="3" age="6"/>
+    <localAd ad={localAdImg} />
     </div>
 
     <div className="bigAd">
-    <BusTimeComp name="1" age="69"/>
-    <BusTimeComp name="2" age = "4"/>
-    <BusTimeComp name="3" age="6"/>
+    <mapComp name="1" age="69"/>
+    <mapComp name="2" age = "4"/>
+    <mapComp name="3" age="6"/>
     </div>
     </div>
   );
