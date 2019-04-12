5784dc8901da358fa89956484b68d6f67792b65f
Will Cruse <willcruse123@gmail.com>
2019-04-12 22:31:30 +0200

    Example Layout

--- bus-shelter/src/index.js
+++ bus-shelter/src/index.js
@@ -1,3 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
+function BusTimeComp(props) {
+  return <h1>{props.name + " " + props.age}</h1>;
+}
+
+function App() {
+  return (
+
+    <div className="busTimes">
+    <BusTimeComp name="1" age="69"/>
+    <BusTimeComp name="2" age = "4"/>
+    <BusTimeComp name="3" age="6"/>
+    </div>
+
+  );
+}
+const element = App();
+
+ReactDOM.render(
+  element,
+  document.getElementById("root")
+);
