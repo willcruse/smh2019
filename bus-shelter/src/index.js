f7b8110790bacdc2e687bb0af77ef7975e37ee2d
Will Cruse <willcruse123@gmail.com>
2019-04-12 23:16:50 +0200

    Front

--- bus-shelter/src/index.js
+++ bus-shelter/src/index.js
@@ -3,22 +3,32 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 function BusTimeComp(props) {
-  return <h1>{props.name + " " + props.age}</h1>;
+  const indexes = [0, 1, 2, 3, 4];
+  const mapper = indexes.map((i) =>
+    (<tr><td>{props.names[i]}</td><td>{props.destinations[i]}</td><td>{props.times[i]}</td></tr>)
+);
+var element = (<table><th>Bus</th><th>Destination</th><th>Time To Arrival</th>{mapper}</table>);
+return element;
 }
 
 function App() {
+  var names = ["U1", "U2", "U3", "U4", "U5"];
+  var destinations = ["University of Bath", "Odd Down", "Can't Spell Uni", "Go To", "Bath Spa"];
+  var times = [1, 2, 3, 4, 5];
   return (
-
+    <div>
     <div className="busTimes">
-    <BusTimeComp name="1" age="69"/>
-    <BusTimeComp name="2" age = "4"/>
-    <BusTimeComp name="3" age="6"/>
+      <BusTimeComp names={names} destinations={destinations} times={times}/>
     </div>
+    {/* <div className="other">
 
+    </div>
+    <div className="map">
+    </div> */}
+  </div>
   );
 }
 const element = App();
-
 ReactDOM.render(
   element,
   document.getElementById("root")
